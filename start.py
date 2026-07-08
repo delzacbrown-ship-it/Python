30315b0e26fe00b429b84f86e6ea8c888809a14c
Delza Brown <delzacbrown@gmail.com>
2026-07-08 01:14:55 +0100

    main

---
+++ start.py
@@ -0,0 +1,71 @@
+# print a message "hello world"
+print("hello world")
+
+# print a number
+print(25)
+
+# declare a variable
+name="Delza"
+print(name) #delza
+
+# make number identities & sum them
+# after we print the sum
+
+number1 = 17+5j
+number2=5.5
+number3= (9)
+
+sum=number1+number2+number3
+bahh=number2+number3
+print(sum)
+print(bahh)
+
+# assign value to site_name variable
+site_name = 'programiz.pro'
+# assign value to site_nam variable
+site_nam = "programiz.pro"
+# assign value to site_na variable
+site_na="programiz.pro"
+
+print(site_name)
+print(site_nam)
+print(site_na)
+
+site_name= "apple.com"
+
+print(site_name)
+a,b,c,d,e= 1,3, 5, "delza", "programiz.pro"
+print(a,b,c,d,e)
+print (a)
+print (b)
+print (c)
+print (d)
+print (e)
+
+
+print('Python is powerful')
+# Display phrase "Python is powerful"
+
+print("Good morning")
+print('It is sunny outside')
+
+# now let us add text to make a single line 
+print("Good morning", end=" ")
+print("It is sunny outside")
+
+Year = 2026
+
+print("The Year", Year, "is half over", Year+1 , "see you soon", sep=" ~ ")
+
+
+print(11 % 8)
+
+num = 11 // 8
+divisor = 11 % 8
+
+print(num, divisor, sep=" r")
+
+x=100
+x+= 20 
+x+= 30
+print(x)
